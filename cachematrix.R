4a054bbe4d4963b7348c5c35bf6f5525e110fe8f
SimonVerhoeven <verhoeven.simon@gmail.com>
2014-07-26 13:56:29 +0200

    Solved programming assignment 2 of the coursera R course

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,68 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R file useful for getting the inverse of a matrix.
+## it returns a cached copy if available
+##
+## sample usage:
+## cachedMatrix <- makeCacheMatrix(matrix(rnorm(25), nrow = 5, ncol = 5))
+## cachedMatrix$get()
+## cacheSolve(cachedMatrix) (needs to be called twice if you want to get a cached copy)
 
-## Write a short comment describing this function
 
+## makeCacheMatrix: returns a list of available functions:
+## - Getter function for the inverse
+## - Setter function for the inverse
+## - Setter function for the inverse
+## - Getter function for the inverse
 makeCacheMatrix <- function(x = matrix()) {
+	#holder object for the inverse
+	inv <- NULL
 
+	#Setter function for thr matrix
+	set <- function(y) {
+		x <<- y
+		inv <<- NULL
+	}
+
+	#Getter function for the matrix
+	get <- function() {
+		x
+	}
+
+	#Setter function for the inverse
+	setmatrix <- function(solve) {
+		inv <<- solve
+	}
+
+	#Getter function for the inverse
+	getmatrix <- function() {
+		inv
+	}
+
+	#Returns a matrix with our defined functions
+ 	list(set = set, 
+		get = get,
+		setmatrix = setmatrix,
+		getmatrix = getmatrix)
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve: Gives us the inverse of the passed matrix.
+##	It returns a cache copy if available, and otherwise determines it
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	# tries to fetch the cached matrix
+	inv <- x$getmatrix()
+	# if available immediately exits the function and informs us that the cached data was used
+	if(!is.null(inv)) {
+		message("getting cached data")
+		return(inv)
+	}
+
+	#determine the inverse
+	data <- x$get()
+	inv <- solve(data, ...)
+
+	#cache it
+	x$setmatrix(inv)
+
+	#return the newly cached inverse
+	inv
 }
